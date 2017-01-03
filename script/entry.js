1ffe810a87aed9b10a9a2fcc7e3bf6a105f76a03
admin@HW-JIAPJI-01 <soullessl@qq.com>
2017-01-03 17:46:16 +0800

    remove bundle.js and add es6 support

--- script/entry.js
+++ script/entry.js
@@ -1,8 +1,8 @@
 // entry.js
 require("../style/style.css");
 require("../style/test.scss");
+require("core.js");
 document.write('It works.');
 document.write(require('./module.js'));
 
-// requirejs
-// document.write(require('./module.js').name);
+Promise.resolve(32).then(x => console.log(x));
