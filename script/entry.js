9422f35c80ca56dda8e3beeb8a308cb577159793
admin@HW-JIAPJI-01 <soullessl@qq.com>
2017-01-12 16:00:16 +0800

    add core.js into package.json

--- script/entry.js
+++ script/entry.js
@@ -2,7 +2,7 @@
 require("../style/style.css");
 require("../style/test.scss");
 require("core.js");
-document.write('It works.');
+document.write('It works!');
 document.write(require('./module.js'));
 
 Promise.resolve(32).then(x => console.log(x));
